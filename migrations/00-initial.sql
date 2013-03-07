a819d9f43aee1a2be8fef10316e030ba7130b545
Alexey Tyagunov <atyaga@gmail.com>
2013-03-07 19:18:07 +0400

    mongo fixtures

--- migrations/00-initial.sql
+++ migrations/00-initial.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS projects;
+--DROP TABLE IF EXISTS projects;
 CREATE TABLE projects (
 	id INT(11) NOT NULL AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL DEFAULT '',
 	PRIMARY KEY (id)
 ) ENGINE=MyISAM DEFAULT CHARSET=UTF8;
 
-DROP TABLE IF EXISTS types;
+--DROP TABLE IF EXISTS types;
 CREATE TABLE types (
 	id INT(11) NOT NULL AUTO_INCREMENT,
 	project_id INT(11) NOT NULL DEFAULT 0,
@@ -15,9 +15,9 @@ CREATE TABLE types (
 	KEY (project_id)
 ) ENGINE=MyISAM DEFAULT CHARSET=UTF8;
 
-DROP TABLE IF EXISTS stats;
+--DROP TABLE IF EXISTS stats;
 CREATE TABLE stats (
-	id INT(11) NOT NULL AUTO_INCREMENT,
+	id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	project_id INT(11) NOT NULL DEFAULT 0,
 	type_id INT(11) NOT NULL DEFAULT 0,
 
@@ -28,20 +28,6 @@ CREATE TABLE stats (
 
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-	PRIMARY KEY (id),
-	
-	KEY t (type_id),
-	KEY p (project_id),
-	KEY mt (meta_type_id),
-	KEY c (created_at),
-	
-	KEY p_t (project_id, type_id),
-	KEY p_mt (project_id, meta_type_id),
-	KEY p_t_mt (project_id, type_id, meta_type_id),
-	
-	KEY p_t_c (project_id, type_id, created_at),
-	KEY p_mt_c (project_id, meta_type_id, created_at),
 	KEY p_t_mt_c (project_id, type_id, meta_type_id, created_at)
-	
 ) ENGINE=MyISAM DEFAULT CHARSET=UTF8;
 
--- test/fixtures.py
+++ test/fixtures.py
@@ -1,35 +1,41 @@
 import MySQLdb
 import string
 import random
+import pymongo
+import sys
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="statstat", charset='utf8')
-cursor = db.cursor()
+is_sql = sys.argv[1] == "mysql";
+
+if is_sql:
+	db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="statstat", charset='utf8')
+else:
+	connection = pymongo.Connection()
+	db = connection.statstat
+	res = db.stats
 
 projects = [1,2,3,4]
 types = [20,21,22,23,24,25,26,27]
 meta_types = [1030, 1031, 1032]
 
 for i in range(0,10000000):
-
-	project_id = random.choice(projects)
-	type_id = random.choice(types)
-	value = random.randint(1,10000)
-
-	sql = """INSERT INTO stats(project_id, type_id, value) VALUES ('%(project_id)s', '%(type_id)s', '%(value)s');
-	"""%{"project_id":project_id, "type_id":type_id, "value":value}
-	print sql
-#	cursor.execute(sql)
+	stat = { "project_id":random.choice(projects), "type_id":random.choice(types), "value":random.randint(1,10000) }
+	if is_sql:
+		sql = """INSERT INTO stats(project_id, type_id, value) VALUES ('%(project_id)s', '%(type_id)s', '%(value)s');"""%stat
+	else:
+		res.insert(stat)
 
 	for j in range(1,3):
-		meta_type_id = random.choice(meta_types)
-		meta_value = random.randint(1,30000)
-		sql = """INSERT INTO stats(project_id, type_id, value, meta_type_id, meta_value) VALUES ('%(project_id)s', '%(type_id)s', '%(value)s', '%(meta_type_id)s', '%(meta_value)s');
-		"""%{"project_id":project_id, "type_id":type_id, "value":value, "meta_type_id":meta_type_id, "meta_value": meta_value}
-		print sql
-#		cursor.execute(sql)
+		stat_meta = { "project_id":random.choice(projects), "type_id":random.choice(types), "value":random.randint(1,10000) , "meta_type_id": random.choice(meta_types), "meta_value": random.randint(1,30000) }
+		if is_sql:
+			sql = sql+"""INSERT INTO stats(project_id, type_id, value, meta_type_id, meta_value) VALUES ('%(project_id)s', '%(type_id)s', '%(value)s', '%(meta_type_id)s', '%(meta_value)s');"""%stat_meta
+		else:
+			res.insert(stat_meta)
 
-	if i % 10000 == 0: 
-		#print i
-		db.commit()
+	if is_sql:
+		res = db.cursor()
+		res.execute(sql)
+
+	if i % 10000 == 0:
+		print i
 
 db.close()
