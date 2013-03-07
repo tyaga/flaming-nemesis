8d23dbf19d3db9cf6e323c8c0d9be1f29c3d5c10
Alexey Tyagunov <atyaga@gmail.com>
2013-03-07 09:47:26 +0400

    http serve

--- gostat/gostat.go
+++ gostat/gostat.go
@@ -1,11 +1,15 @@
 package main
 
 import "fmt"
+import "log"
 import "net/http"
 
 func main() {
     http.HandleFunc("/stat", func(w http.ResponseWriter, r *http.Request) {
-		//fmt.Fprintln(w, "Hello, world.")
+        log.Printf("[gostat] Got stat: %s", r.URL.RawQuery)
+        fmt.Println(r.URL.ParseQuery());
+
+		fmt.Fprint(w, "stat got stat")
     })
     http.ListenAndServe(":8090", nil)
 }
